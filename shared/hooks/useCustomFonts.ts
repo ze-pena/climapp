4ea2a9b02c0ac8959990f47694915d8ef72e09ae
ze-pena <jose.pena.dev@gmail.com>
2025-08-31 21:53:00 -0300

    feat: create custom fonts hook to add google external fonts

---
+++ shared/hooks/useCustomFonts.ts
@@ -0,0 +1,20 @@
+import {
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  useFonts,
+} from "@expo-google-fonts/montserrat";
+
+export default function useCustomFonts() {
+  const [isLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+
+  if (!isLoaded) return false;
+
+  return isLoaded;
+}
