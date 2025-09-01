f44351e6609de63723a6d960b8c3d2fa5e5549c8
ze-pena <jose.pena.dev@gmail.com>
2025-08-31 21:56:28 -0300

    feat: configure layout and create the main app screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
+import useCustomFonts from "@/shared/hooks/useCustomFonts";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  const isLoaded = useCustomFonts();
+
+  if (!isLoaded) return <></>;
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="cities/index" options={{ headerShown: false }} />
+      <Stack.Screen name="cities/[name]" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,59 @@
-import { Text, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
+import { Image, StyleSheet, Text, TouchableOpacity } from "react-native";
 
-export default function Index() {
+export default function Home() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <LinearGradient colors={["#00457D", "#05051F"]} style={styles.container}>
+      <Image source={require("@/assets/images/home/logo.png")} />
+
+      <Image source={require("@/assets/images/home/ilustration.png")} />
+
+      <Text style={styles.title}>Boas vindas!</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/cities/index")}
+      >
+        <Text style={styles.buttonText}>Entrar</Text>
+
+        <MaterialIcons name="arrow-forward" size={24} color="#01080E" />
+      </TouchableOpacity>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 64,
+    paddingVertical: 80,
+    paddingHorizontal: 48,
+  },
+  title: {
+    fontSize: 24,
+    color: "#fff",
+    fontFamily: "Montserrat_600SemiBold",
+  },
+  button: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 56,
+    backgroundColor: "#7693FF",
+    borderRadius: 32,
+    flexDirection: "row",
+    gap: 16,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#01080E",
+    fontWeight: "600",
+    fontFamily: "Montserrat_600SemiBold",
+  },
+});
